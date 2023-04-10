c57fe800d0c6b1eab48ac6c0b5195fbd6a1de025
codejoes <114871953+codejoes@users.noreply.github.com>
2023-04-10 12:47:13 -0400

    update package.json, add scrolltotop hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,44 @@
+import { AnimatePresence } from "framer-motion";
 import React from "react";
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import Home from "./pages/Home";
 import Matchup from "./pages/Matchup";
 import Vote from "./pages/Vote";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
 
-// const client = new ApolloClient({
-//   uri: "/graphql",
-//   cache: new InMemoryCache(),
-// });
+const About = lazy(() => import("./pages/AboutMe"));
+const Contact = lazy(() => import("./pages/Contact.jsx"));
+const Home = lazy(() => import("./pages/Home"));
+const Projects = lazy(() => import("./pages/Projects"));
+const ProjectSingle = lazy(() => import("./pages/ProjectSingle.jsx"));
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div className="flex-column justify-center align-center min-100-vh bg-primary">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/matchup" element={<Matchup />} />
-            <Route path="/matchup/:id" element={<Vote />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </Router>
-    </ApolloProvider>
+    <AnimatePresence>
+      <div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
+        <Router>
+          <ScrollToTop />
+          <AppHeader />
+          <Suspense fallback={""}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="projects" element={<Projects />} />
+              <Route
+                path="projects/single-project"
+                element={<ProjectSingle />}
+              />
+
+              <Route path="about" element={<About />} />
+              <Route path="contact" element={<Contact />} />
+            </Routes>
+          </Suspense>
+          <AppFooter />
+        </Router>
+        <UseScrollToTop />
+      </div>
+    </AnimatePresence>
   );
 }
 
